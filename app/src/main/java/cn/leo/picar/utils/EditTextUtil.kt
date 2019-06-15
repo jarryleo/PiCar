371b4a359f51f6bc5b29bffc16a1c8c64330738e
yjtx256@qq.com <yjtx256@qq.com>
2019-06-15 16:54:02 +0800

    输入框数字检测

--- app/src/main/java/cn/leo/picar/utils/EditTextUtil.kt
+++ app/src/main/java/cn/leo/picar/utils/EditTextUtil.kt
@@ -1,6 +1,16 @@
 package cn.leo.picar.utils
 
+import android.text.TextUtils
 import android.widget.EditText
 
 
-fun EditText.getInt() = this.text.toString().toInt()
+fun EditText.getInt(): Int {
+    val s = this.text.toString()
+    if(TextUtils.isEmpty(s)){
+        return 0
+    }
+    if (TextUtils.isDigitsOnly(s)){
+        return s.toInt()
+    }
+    return 0
+}
