1979dc5b6e4d9388028738a1f6a51193f7ad19c6
yjtx256@qq.com <yjtx256@qq.com>
2019-06-18 17:25:44 +0800

    摇杆控件解析数据完成

--- app/src/main/java/cn/leo/picar/view/RockerParser.kt
+++ app/src/main/java/cn/leo/picar/view/RockerParser.kt
@@ -1,30 +1,45 @@
 package cn.leo.picar.view
 
 import android.widget.SeekBar
-import kotlin.math.abs
-import kotlin.math.tan
+import kotlin.math.acos
+import kotlin.math.sqrt
 
 object RockerParser {
-    const val pi = Math.PI
     fun parseRocker(rockerView: RockerView, sb: SeekBar, parser: (IntArray) -> Unit) {
         val arr = IntArray(8)
         rockerView.setRockerListener { x, y ->
             val speed = sb.progress
-            arr[0] = 0
-            arr[1] = 0
-            arr[2] = 0
-            arr[3] = 0
-            arr[4] = 0
-            arr[5] = 0
-            arr[6] = 0
-            arr[7] = 0
-            if (y != 0) {
-                println(" x = $x  y = $y")
-                val arc = tan(x.toDouble() / -y.toDouble()) * 180 / pi
-                println(arc)
+            when (angle(x, -y) - 22) {
+                in 0 until 45 -> setArr(arr, speed, 0, 2, 4, 8)
+                in 45 until 90 -> setArr(arr, speed, 0, 6)
+                in 90 until 135 -> setArr(arr, speed, 0, 3, 5, 6)
+                in 135 until 180 -> setArr(arr, speed, 3, 5)
+                in 180 until 225 -> setArr(arr, speed, 1, 3, 5, 7)
+                in 225 until 270 -> setArr(arr, speed, 1, 7)
+                in 270 until 315 -> setArr(arr, speed, 1, 2, 4, 7)
+                in 315 until 360 -> setArr(arr, speed, 2, 4)
             }
             parser(arr)
         }
     }
 
+    fun setArr(arr: IntArray, speed: Int, vararg indexs: Int) {
+        arr.forEachIndexed { index, _ ->
+            if (indexs.contains(index)) {
+                arr[index] = speed
+            } else {
+                arr[index] = 0
+            }
+        }
+    }
+
+
+    private fun angle(x: Int, y: Int): Int {
+        val len = sqrt((x * x + y * y).toDouble())
+        var angle = acos(y / len)
+        if (x < 0) {
+            angle = 2 * Math.PI - angle
+        }
+        return (angle * 180 / Math.PI).toInt()
+    }
 }
--- app/src/test/java/cn/leo/picar/ExampleUnitTest.kt
+++ app/src/test/java/cn/leo/picar/ExampleUnitTest.kt
@@ -5,9 +5,12 @@ import cn.leo.picar.cmd.CommandType
 import cn.leo.picar.msg.BaseMsg
 import cn.leo.picar.msg.MsgType
 import cn.leo.picar.utils.JsonUtil
+import cn.leo.picar.view.RockerParser
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.util.*
+import kotlin.math.acos
+import kotlin.math.sqrt
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -21,7 +24,7 @@ class ExampleUnitTest {
     }
 
     @Test
-    fun test1(){
+    fun test1() {
         val msg = BaseMsg<Command>()
         msg.type = MsgType.TYPE_CAR
         msg.data = Command(CommandType.IDLE, 0)
@@ -31,5 +34,31 @@ class ExampleUnitTest {
     private fun sendMsg(msg: BaseMsg<*>) {
         val toJson = JsonUtil.toJson(msg)
         println(toJson)
+    }
+
+    @Test
+    fun test2(){
+        println(angle(0,100))
+        println(angle(70,70))
+        println(angle(100,0))
+        println(angle(70,-70))
+        println(angle(0,-100))
+        println(angle(-70,-70))
+        println(angle(-100,0))
+        println(angle(-70,70))
+
+        val arr = IntArray(8)
+        RockerParser.setArr(arr,80,0,2,4,6)
+        println(Arrays.toString(arr))
+    }
+
+
+    private fun angle(x: Int, y: Int) :Int{
+        val len = sqrt((x * x + y * y).toDouble())
+        var angle = acos(y / len)
+        if (x < 0){
+            angle = 2*Math.PI - angle
+        }
+        return (angle * 180 / Math.PI).toInt()
     }
 }
